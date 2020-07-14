7c5f27bb2171241275d57187b6980e5044635cf4
JulzV8 <jevallejos99@gmail.com>
2020-07-13 22:21:29 -0300

    Dia 1 EJ1/6

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,10 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var dato;
+	
+	var dato = prompt("Ingrese un dato");
+	
+	alert(dato);
+	
 }
-
